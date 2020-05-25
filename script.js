44849f960c5a8206dd86cfc4b4cb9f86d6420128
dmartinez1100 <davidfel.martinez@urosario.edu.co>
2020-05-25 00:33:06 -0500

    ds

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ vec_preg = ['{"texto":"DECRETO 633 DE 2017:<br>Artículo 1°.- Delegar en los Al
 
 '{"texto":"" ,"enunciado": "los Alcaldes Locales tienen la facultad para ejercer en el ámbito de su territorio las funciones administrativas de inspección, control y vigilancia consagradas en el artículo 62 de la Ley 1480 de 2011.","respuestas": {"a": "a) Verdadero  ","b":"b) Falso","c":"","d":""},"resp_c": "b","imagen":""}',
 
-'{"texto":"" ,"enunciado": "De conformidad con los siguientes links:  y , En Colombia que productos tienen regulado su precio actualmente:","respuestas": {"a": "a) Leche cruda, arroz, huevos, productos agropecuarios y gasolina","b":"b) Gasolina, medicamentos, leche cruda y productos agropecuarios","c":"c) Arroz, gasolina, productos agropecuarios y huevos","d":"d) Huevos, medicamentos, gasolina y leche cruda"},"resp_c": "b","imagen":"homero.gif","link":{"URL":["https://www.portafolio.co/economia/gobierno/gobierno-reduce-precio-de-la-gasolina-en-el-pais-539104","https://www.sic.gov.co/control-de-precios"],"texto":["txt1","txt2"],"pos":[41,44]}}',
+'{"texto":"" ,"enunciado": "De conformidad con los siguientes links:  y , En Colombia que productos tienen regulado su precio actualmente:","respuestas": {"a": "a) Leche cruda, arroz, huevos, productos agropecuarios y gasolina","b":"b) Gasolina, medicamentos, leche cruda y productos agropecuarios","c":"c) Arroz, gasolina, productos agropecuarios y huevos","d":"d) Huevos, medicamentos, gasolina y leche cruda"},"resp_c": "b","imagen":"homero.gif","link":{"URL":["https://www.portafolio.co/economia/gobierno/gobierno-reduce-precio-de-la-gasolina-en-el-pais-539104","https://www.sic.gov.co/control-de-precios"],"texto":["Link 1","Link 2"],"pos":[41,44]}}',
 
 '{"texto":"https://www.youtube.com/watch?v=z7t8CJTLuFM" ,"enunciado": "4- ¿El acaparamiento y la especulación son delitos tipificados en el código penal?","respuestas": {"a": "a) Falso","b":"b) Verdadero","c":"","d":""},"resp_c": "b","imagen":""}',
 
@@ -10,7 +10,7 @@ vec_preg = ['{"texto":"DECRETO 633 DE 2017:<br>Artículo 1°.- Delegar en los Al
 
 '{"texto":"" ,"enunciado": "La definición El productor, fabricante o distribuidor mayorista que ponga en venta artículo o género oficialmente considerado como de primera necesidad a precios superiores a los fijados por autoridad competente corresponde a:","respuestas": {"a": "a) Acaparamiento","b":"b) Especulación","c":"c) Abuso de poder","d":"d) Posición dominante"},"resp_c": "b","imagen":""}',
 
-'{"texto":"" ,"enunciado": "De conformidad con el siguiente link: , La resolución 078 de 2020 establece como productos de primera necesidad: ","respuestas": {"a": "a) Acetaminofén, arveja, arroz, leche, naranjas, tapabocas, naproxeno, ivermectina y queso doble crema.","b":"b) Guantes para examen, medidores de temperatura, cebolla larga, papa negra, azitromicina, jabón de tocador en barra y mantequilla.","c":"c) Sal, azúcar, papa criolla, huevos, ibuprofeno, mascarilla quirúrgica, guantes para examen y cloroquina.","d":"d) Cebolla en rama, naranja, arveja verde, amoxicilina, ibuprofeno, naproxeno,mascarilla quirúrgica y arroz"},"resp_c": "d","imagen":"frutas.jpg","link":{"URL":["https://www.sic.gov.co/sites/default/files/documentos/042020/RESOLUCIO%CC%81N%20NO_%20000078%20DE%202020.pdf"],"texto":["txt1"],"pos":[37]}}',
+'{"texto":"" ,"enunciado": "De conformidad con el siguiente link: , establece como productos de primera necesidad: ","respuestas": {"a": "a) Acetaminofén, arveja, arroz, leche, naranjas, tapabocas, naproxeno, ivermectina y queso doble crema.","b":"b) Guantes para examen, medidores de temperatura, cebolla larga, papa negra, azitromicina, jabón de tocador en barra y mantequilla.","c":"c) Sal, azúcar, papa criolla, huevos, ibuprofeno, mascarilla quirúrgica, guantes para examen y cloroquina.","d":"d) Cebolla en rama, naranja, arveja verde, amoxicilina, ibuprofeno, naproxeno,mascarilla quirúrgica y arroz"},"resp_c": "d","imagen":"frutas.jpg","link":{"URL":["https://www.sic.gov.co/sites/default/files/documentos/042020/RESOLUCIO%CC%81N%20NO_%20000078%20DE%202020.pdf"],"texto":["La resolución 078 de 2020"],"pos":[38]}}',
 
 '{"texto":"" ,"enunciado": "¿Teniendo en cuenta que en Colombia hay libertad de mercado y solo hay ciertos productos en que su precio es fijado por el gobierno nacional, La resolución 078 de 2020 puede fijar precios a los productos de primera necesidad?","respuestas": {"a": "a) La resolución 078 de 2020 establece los precios de 26 productos de primera necesidad por orden del gobierno nacional debido a que con ocasión de la emergencia los proveedores han aprovechado en el alza de los mismos.","b":"b) En Colombia hay libertad de mercado y por ende los precios de los productos no pueden ser fijados por el gobierno nacional a excepción de la gasolina, la leche cruda, medicamentos y productos agropecuarios.","c":"c) La resolución 078 de 2020 establece 23 productos de primera necesidad, pero no fija sus precios porque en Colombia hay libertad de mercado, por lo tanto, los distribuidores pueden colocar el precio que consideren razonable.","d":"d) La resolución 078 de 2020 establece 26 productos de primera necesidad, pero no fija sus precios. aunque en Colombia hay libertad de mercado el gobierno si puede fijar de forma excepcional el precio de algunos productos, por lo tanto, se está a la espera de la fijación del precio de estos 26 productos de primera necesidad"},"resp_c": "d","imagen":""}',
 
@@ -18,7 +18,10 @@ vec_preg = ['{"texto":"DECRETO 633 DE 2017:<br>Artículo 1°.- Delegar en los Al
 
 '{"texto":"" ,"enunciado": "Qué instituciones pueden realizar Metrología Legal.","respuestas": {"a": "a) La Superintendencia de Industria y Comercio.","b":"b) Las alcaldías.","c":"c) Organismos Autorizados de Verificación Metrológica OAVM. ","d":"d) Todas las anteriores. "},"resp_c": "d","imagen":""}',
 
-'{"texto":"¿Sabes qué es el PUM?<br/><br/>Es el Precio por Unidad de Medida, es decir que el comerciante debe dar a conocer al consumidor el costo de cada producto según sus características en: gramos, kilogramos, litros, mililitros, centímetros y metros o unidad según sea cada caso.<br><br/>Esto quiere decir que los establecimientos comerciales deben indicar cuál es el precio de cada artículo según las condiciones de pesaje del mismo, por ejemplo si usted compra una libra de carne, papa o azúcar, se debe indicar el PUM de un gramo o kilogramo, para el caso de un litro de leche, gaseosa u otro similar el PUM será indicado en litros o mililitros, en algunos casos simplemente se relaciona por unidad como puede ser el caso de algunas frutas o huevos entre otros.<br><br/>Entonces el PUM es fundamental para poder comparar el precio de los productos, por ejemplo una botella de agua de 500 ml cuesta $1.000 pesos, mientras  que una botella de 1 litro (1.000 ml)  cuesta $1.500 pesos; en el primer caso el PUM es de $2 pesos el ml, y en el segundo caso es de $1.5 el ml,  el cálculo se realiza simplemente dividiendo el precio del prodecto entre la cantidad de medida.<br><br/> El PUM deberá encontrase en etiquetas, empaques, adhesivos, los bienes mismos, envases y las góndolas o anaqueles de exhibición." ,"enunciado": "En Colombia el PUM para gasolina se mide por galones, si compras 2.5 galones de gasolina por valor de $25.000 pesos. Cuál es el Precio por Unidad de Medida de la gasolina?","respuestas": {"a": "a) $2.500 por galón.","b":"b) $10.000 por galón.","c":"c) $10.500 por galón.","d":"$12.500 por galón."},"resp_c": "b","imagen":"gasolina.gif"}']
+'{"texto":"¿Sabes qué es el PUM?<br/><br/>Es el Precio por Unidad de Medida, es decir que el comerciante debe dar a conocer al consumidor el costo de cada producto según sus características en: gramos, kilogramos, litros, mililitros, centímetros y metros o unidad según sea cada caso.<br><br/>Esto quiere decir que los establecimientos comerciales deben indicar cuál es el precio de cada artículo según las condiciones de pesaje del mismo, por ejemplo si usted compra una libra de carne, papa o azúcar, se debe indicar el PUM de un gramo o kilogramo, para el caso de un litro de leche, gaseosa u otro similar el PUM será indicado en litros o mililitros, en algunos casos simplemente se relaciona por unidad como puede ser el caso de algunas frutas o huevos entre otros.<br><br/>Entonces el PUM es fundamental para poder comparar el precio de los productos, por ejemplo una botella de agua de 500 ml cuesta $1.000 pesos, mientras  que una botella de 1 litro (1.000 ml)  cuesta $1.500 pesos; en el primer caso el PUM es de $2 pesos el ml, y en el segundo caso es de $1.5 el ml,  el cálculo se realiza simplemente dividiendo el precio del prodecto entre la cantidad de medida.<br><br/> El PUM deberá encontrase en etiquetas, empaques, adhesivos, los bienes mismos, envases y las góndolas o anaqueles de exhibición." ,"enunciado": "En Colombia el PUM para gasolina se mide por galones, si compras 2.5 galones de gasolina por valor de $25.000 pesos. Cuál es el Precio por Unidad de Medida de la gasolina?","respuestas": {"a": "a) $2.500 por galón.","b":"b) $10.000 por galón.","c":"c) $10.500 por galón.","d":"$12.500 por galón."},"resp_c": "b","imagen":"gasolina.gif"}',
+
+'{"texto":"REGLAMENTOS TECNICOS<br><br>Se define como el documento en el que se establecen las características de un producto o los procesos y métodos de producción con ellas relacionados, con inclusión de las disposiciones administrativas aplicables, y cuya observancia es obligatoria.<br><br>En la actualidad se disponen de RT en las siguientes categorías:<br> &nbsp;&nbsp;Productos de Consumo Personal<br>&nbsp;&nbsp;&nbsp;&nbsp;Etiquetas de calzado.<br>&nbsp;&nbsp;&nbsp;&nbsp;Etiquetado de confecciones.<br>&nbsp;&nbsp;&nbsp;&nbsp;Juguetes y sus accesorios.<br><br>&nbsp;&nbsp;Productos de Consumo en el Hogar<br>&nbsp;&nbsp;&nbsp;&nbsp;Aplicable a la etiqueta de baldosas cerámicas, que se produzcan, importen y comercialicen en Colombia.<br>&nbsp;&nbsp;&nbsp;&nbsp;De emergencia para los utensilios de vidrio, cerámica y vitrocerámica, las vajillas de vidrio, cerámica y vitrocerámica en contacto con alimentos, y los utensilios de cerámica empleados en la cocción de los alimentos.<br>&nbsp;&nbsp;&nbsp;&nbsp;Ollas de Presión de uso doméstico que se importen o produzcan nacionalmente para su comercialización en Colombia.<br>&nbsp;&nbsp;&nbsp;&nbsp;Aplicable a la etiqueta de pinturas base agua tipo emulsión de uso arquitectónico, para interiores o exteriores, que se fabriquen, importen o comercialicen en Colombia.<br>&nbsp;&nbsp;&nbsp;&nbsp;Etiquetado de baldosas cerámicas<br>&nbsp;&nbsp;&nbsp;&nbsp;Etiquetado de Pilas de Zinc-Carbón y Alcalinas.<br><br>" ,"enunciado": "En Colombia el PUM para gasolina se mide por galones, si compras 2.5 galones de gasolina por valor de $25.000 pesos. Cuál es el Precio por Unidad de Medida de la gasolina?","respuestas": {"a": "a) $2.500 por galón.","b":"b) $10.000 por galón.","c":"c) $10.500 por galón.","d":"$12.500 por galón."},"resp_c": "b","imagen":"gasolina.gif"}'
+]
 
 preguntas = 0
 respuestas = []
@@ -33,19 +36,37 @@ console.log(respuestas)
 
 function comprobar_resp(){
     var inputs = []
+    var flag = true
+
+    nombre = "David Martinez"
+    var URL = 'bien.html?name='+encodeURIComponent(nombre);
+    window.location.href = URL;
     for(var i=1;i<=preguntas;i++){
         elemento = document.querySelector('input[name="'+i+'"]:checked')
         if (elemento == null){
             document.getElementById('mensaje'+i).innerHTML += '<p>No respondiste esta pregunta</p>'
+            flag = false
             }
         else{
             elemento = elemento.value
-        inputs.push(elemento)}
+            inputs.push(elemento)}
     }
-    if(JSON.stringify(respuestas)==JSON.stringify(inputs)){
-        console.log("Correcto")
+    console.log(flag)
+    if(flag){
+        if(JSON.stringify(respuestas)==JSON.stringify(inputs)){
+            console.log("Correcto")
+            console.log("falso")
+            nombre = "David Martinez"
+            var URL = 'bien.html?name='+encodeURIComponent(nombre);
+            window.location.href = URL;
+        }
+        else {
+            console.log("falso")
+            nombre = "David Martinez"
+            var URL = 'mal.html?name='+encodeURIComponent(nombre);
+            window.location.href = URL;
+        }
     }
-    else console.log("falso")
 }
 
 
@@ -79,7 +100,6 @@ function new_qest(texto,enunciado,a,b,c,d,respuesta,imagen,link){
 }
 
 function pres(num){
-    console.log("me presionaron"+num)
     elemento = document.getElementById('mensaje'+num)
     elemento.innerHTML = ""
 }
