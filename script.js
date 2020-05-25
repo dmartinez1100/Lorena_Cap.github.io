a0899f6b2d0e08dfe6a6e1512190527d5775cc53
dmartinez1100 <davidfel.martinez@urosario.edu.co>
2020-05-24 21:45:35 -0500

    mas_avance

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
+vec_preg = ['{"texto":"DECRETO 633 DE 2017:<br>Artículo 1°.- Delegar en los Alcaldes Locales la facultad para ejercer en el ámbito de su territorio las funciones administrativas de control y vigilancia consagradas en el artículo 62 de la Ley 1480 de 2011 de acuerdo al procedimiento señalado en el artículo 60 ibídem.Parágrafo. En los términos de la Ley 1480 del 2011, la delegación realizada en el presente Decreto es complementarla a la facultad consagrada en el numeral 12 del artículo 86 del Decreto Ley 1421 de 1993, para el efectivo control de precios, pesas y medidas, y el ejercicio de las acciones necesarias para evitar o sancionar el acaparamiento y la especulación.Artículo 2°.- El presente Decreto rige a partir del día siguiente de su publicación." ,"enunciado": "De conformidad con el decreto 633 de 2017 la alcaldía mayor de Bogotá delego a los alcaldes locales facultades respecto:","respuestas": {"a": "a) control de precios, pesas y medidas, el acaparamiento y la especulación.","b":"b) Pesas, medidas, el acaparamiento y la especulación.","c":"c) Pesas, medidas, garantía, publicidad engañosa y control de precios.","d":"d) Publicidad engañosa, control de precios y medidas."},"resp_c": "a","imagen":"alcalde.gif"}',
+
+'{"texto":"" ,"enunciado": "los Alcaldes Locales tienen la facultad para ejercer en el ámbito de su territorio las funciones administrativas de inspección, control y vigilancia consagradas en el artículo 62 de la Ley 1480 de 2011.","respuestas": {"a": "a) Verdadero  ","b":"b) Falso","c":"","d":""},"resp_c": "b","imagen":""}',
+
+'{"texto":"" ,"enunciado": "De conformidad con los siguientes links: https://www.portafolio.co/economia/gobierno/gobierno-reduce-precio-de-la-gasolina-en-el-pais-539104 y https://www.sic.gov.co/control-de-precios, En Colombia que productos tienen regulado su precio actualmente:","respuestas": {"a": "a) Leche cruda, arroz, huevos, productos agropecuarios y gasolina","b":"b) Gasolina, medicamentos, leche cruda y productos agropecuarios","c":"c) Arroz, gasolina, productos agropecuarios y huevos","d":"d) Huevos, medicamentos, gasolina y leche cruda"},"resp_c": "b","imagen":"homero.gif"}',
+
+'{"texto":"https://www.youtube.com/watch?v=z7t8CJTLuFM" ,"enunciado": "4- ¿El acaparamiento y la especulación son delitos tipificados en el código penal?","respuestas": {"a": "a) Falso","b":"b) Verdadero","c":"","d":""},"resp_c": "b","imagen":""}',
+
+'{"texto":"" ,"enunciado": "La definición el sustraer del comercio artículo o producto oficialmente considerado de primera necesidad corresponde a:","respuestas": {"a": "a)Acaparamiento","b":"b) Especulación","c":"c) Abuso de poder","d":"d) Posición dominante"},"resp_c": "a","imagen":"locos.gif"}',
+
+'{"texto":"" ,"enunciado": "La definición El productor, fabricante o distribuidor mayorista que ponga en venta artículo o género oficialmente considerado como de primera necesidad a precios superiores a los fijados por autoridad competente corresponde a:","respuestas": {"a": "a) Acaparamiento","b":"b) Especulación","c":"c) Abuso de poder","d":"d) Posición dominante"},"resp_c": "b","imagen":""}',
+
+'{"texto":"" ,"enunciado": "De conformidad con el siguiente link: https://www.sic.gov.co/sites/default/files/documentos/042020/RESOLUCIO%CC%81N%20NO_%20000078%20DE%202020.pdf , La resolución 078 de 2020 establece como productos de primera necesidad: ","respuestas": {"a": "a) Acetaminofén, arveja, arroz, leche, naranjas, tapabocas, naproxeno, ivermectina y queso doble crema.","b":"b) Guantes para examen, medidores de temperatura, cebolla larga, papa negra, azitromicina, jabón de tocador en barra y mantequilla.","c":"c) Sal, azúcar, papa criolla, huevos, ibuprofeno, mascarilla quirúrgica, guantes para examen y cloroquina.","d":"d) Cebolla en rama, naranja, arveja verde, amoxicilina, ibuprofeno, naproxeno,mascarilla quirúrgica y arroz"},"resp_c": "d","imagen":"frutas.jpg"}',
+
+'{"texto":"" ,"enunciado": "¿Teniendo en cuenta que en Colombia hay libertad de mercado y solo hay ciertos productos en que su precio es fijado por el gobierno nacional, La resolución 078 de 2020 puede fijar precios a los productos de primera necesidad?","respuestas": {"a": "a) La resolución 078 de 2020 establece los precios de 26 productos de primera necesidad por orden del gobierno nacional debido a que con ocasión de la emergencia los proveedores han aprovechado en el alza de los mismos.","b":"b) En Colombia hay libertad de mercado y por ende los precios de los productos no pueden ser fijados por el gobierno nacional a excepción de la gasolina, la leche cruda, medicamentos y productos agropecuarios.","c":"c) La resolución 078 de 2020 establece 23 productos de primera necesidad, pero no fija sus precios porque en Colombia hay libertad de mercado, por lo tanto, los distribuidores pueden colocar el precio que consideren razonable.","d":"d) La resolución 078 de 2020 establece 26 productos de primera necesidad, pero no fija sus precios. aunque en Colombia hay libertad de mercado el gobierno si puede fijar de forma excepcional el precio de algunos productos, por lo tanto, se está a la espera de la fijación del precio de estos 26 productos de primera necesidad"},"resp_c": "d","imagen":""}',
+
+'{"texto":"METROLOGÍA LEGAL<br/>Facultades a las alcaldías.<br/><br/>Actividades que se derivan de los requisitos legales que se aplican a la medición, las unidades de medida, los instrumentos de medida y los métodos de medida que se llevan a cabo por los organismos competentes. <br/>Las verificaciones metrológicas, se adelantan por parte de la Superintendencia de Industria y Comercio, y las alcaldías, directamente o con el apoyo de Organismos Autorizados de Verificación Metrológica –OAVM.<br/>En materia de IVC, se realizan únicamente a establecimientos comerciales que transen con combustibles líquidos derivados del petróleo y cualquier establecimiento que realice transacciones comerciales con balanzas <br/>como por ejemplo alimentos a granel, frutas, verduras, carnes, quesos, etc.<br/><br/>La Protección de los consumidores, se basa en aspectos relacionados con:<br/>1.Velar por la integridad de los productos, bienes y servicios que garanticen la salud, seguridad, medio ambiente e intereses económicos.<br/>2. Asegurar que los instrumentos de medición se ajusten a la  Metrología legal según sea el caso para procurar la exactitud conveniente en las transacciones comerciales.<br/>3. Verificar  que se cumplan los Reglamentos Técnicos estipulados.<br/>4. Facilitar el intercambio comercial nacional e internacional.<br/>5. Prevenir la ocurrencia de fraude." ,"enunciado": "9. Indique a cuales establecimientos se realiza Metrología Legal:","respuestas": {"a": "a- Estaciones de servicio de Gasolina.","b":"b- Establecimientos comerciales con Balanzas.","c":"c- Cualquier establecimiento comercial.","d":"d- Sólo A y B son verdaderas"},"resp_c": "d","imagen":"IMG-001.jpg"}',
+
+'{"texto":"" ,"enunciado": "Qué instituciones pueden realizar Metrología Legal.","respuestas": {"a": "a) La Superintendencia de Industria y Comercio.","b":"b) Las alcaldías.","c":"c) Organismos Autorizados de Verificación Metrológica OAVM. ","d":"d) Todas las anteriores. "},"resp_c": "d","imagen":""}',
+
+'{"texto":"¿Sabes qué es el PUM?<br/><br/>Es el Precio por Unidad de Medida, es decir que el comerciante debe dar a conocer al consumidor el costo de cada producto según sus características en: gramos, kilogramos, litros, mililitros, centímetros y metros o unidad según sea cada caso.<br/>Esto quiere decir que los establecimientos comerciales deben indicar cuál es el precio de cada artículo según las condiciones de pesaje del mismo, por ejemplo si usted compra una libra de carne, papa o azúcar, se debe indicar el PUM de un gramo o kilogramo, para el caso de un litro de leche, gaseosa u otro similar el PUM será indicado en litros o mililitros, en algunos casos simplemente se relaciona por unidad como puede ser el caso de algunas frutas o huevos entre otros.<br/>Entonces el PUM es fundamental para poder comparar el precio de los productos, por ejemplo una botella de agua de 500 ml cuesta $1.000 pesos, mientras  que una botella de 1 litro (1.000 ml)  cuesta $1.500 pesos; en el primer caso el PUM es de $2 pesos el ml,<br/> y en el segundo caso es de $1.5 el ml,  el cálculo se realiza simplemente dividiendo el precio del prodecto entre la cantidad de medida.<br/> El PUM deberá encontrase en etiquetas, empaques, adhesivos, los bienes mismos, envases y las góndolas o anaqueles de exhibición." ,"enunciado": "En Colombia el PUM para gasolina se mide por galones, si compras 2.5 galones de gasolina por valor de $25.000 pesos. Cuál es el Precio por Unidad de Medida de la gasolina?","respuestas": {"a": "a) $2.500 por galón.","b":"b) $10.000 por galón.","c":"c) $10.500 por galón.","d":"$12.500 por galón."},"resp_c": "b","imagen":"gasolina.gif"}']
+
 preguntas = 0
 respuestas = []
-preguntas = ['']
 
-//pr = JSON.parse('{"texto":"" ,"enunciado": true,"preguntas": {"a": "asdad","b":"wfwqfq","c":"fsafa","d":"safawsg"},"respuesta": "nu","imagen":true}')
-pr2 = ['{"texto":"" ,"enunciado": "","respuestas": {"a": "","b":"","c":"","d":""},"resp_c": "","imagen":".gif"}',
-'{"texto":"" ,"enunciado": "","respuestas": {"a": "","b":"","c":"","d":""},"resp_c": "","imagen":".gif"}'
-
-]
-
-console.log(pr2[1])
-elemento = document.getElementById("1")
-
-//JSON.stringify(a)==JSON.stringify(b)
-
-//elemento.onclick = () => {console.log(elemento.value)}
-console.log(elemento)
-
+for(var i=0;i<vec_preg.length;i++){
+    console.log(i)
+    console.log(vec_preg[i])
+    j = JSON.parse(vec_preg[i])
+    new_qest(j.texto,j.enunciado,j.respuestas.a,j.respuestas.b,j.respuestas.c,j.respuestas.d,j.resp_c,j.imagen)
+}
+console.log(respuestas)
 function resp(){
     var inputs = []
     for(var i=1;i<=preguntas;i++){
@@ -32,6 +46,10 @@ function resp(){
     else console.log("falso")
 }
 
+//'+'<div class='texto'>texto</div>'+'
+
+//'+'width="100%"'+'
+
 function new_qest(texto,enunciado,a,b,c,d,respuesta,imagen){
     container = document.getElementById('cuerpo')
     num = preguntas+1;
@@ -42,15 +60,11 @@ function new_qest(texto,enunciado,a,b,c,d,respuesta,imagen){
         clase_i = "respuestas_c";
         imagen = "assets/img/"+imagen
     }
-    console.log(clase_i)
-    code1 = '<div class="pregunta">'+'<div class="texto">'+texto+'</div>'+'<div '+'class = "enunciado"'+' >'+num+'. '+enunciado+'</div><div class="'+clase_i+'">'+'<div class="respuestas"><div class="respuesta">'+'<input '+' id=a'+num+''+' type="radio" name="'+num+'" value="a"><label class="respuesta_txt"'+'width="100%"'+'for="a'+num+'">'+a+'</label>'+'</div>'+'<br>'+'<div class="respuesta">'+'<input '+' id=b'+num+''+' type="radio" name="'+num+'" value="b"><label class="respuesta_txt" for="b'+num+'">'+b+'</label>'+'</div>'+'<br>'+'<div class="respuesta">'+'<input '+' id=c'+num+''+' type="radio"name="'+num+'" value="c"><label class="respuesta_txt" for="c'+num+'">'+c+'</label>'+'</div>'+'<br>'+'<div class="respuesta">'+'<input '+' id=d'+num+''+' type="radio" name="'+num+'" value="d"><label class="respuesta_txt"for="d'+num+'">'+d+'</label>'+'</div></div>'+'<div class="imagen"><img src="'+imagen+'"alt=""></div></div></div>';
+    code1 = '<div class="pregunta">'+'<div class="texto">'+texto+'</div>'+'<div '+'class = "enunciado"'+' >'+num+'. '+enunciado+'</div><div '+' id=mensaje'+num+''+' class="mensaje"></div><div class="'+clase_i+'">'+'<div class="respuestas"><div class="respuesta">'+'<input '+' id=a'+num+''+' type="radio" name="'+num+'" value="a"><label class="respuesta_txt"'+'width="100%"'+'for="a'+num+'">'+a+'</label>'+'</div>'+'<br>'+'<div class="respuesta">'+'<input '+' id=b'+num+''+' type="radio" name="'+num+'" value="b"><label class="respuesta_txt" for="b'+num+'">'+b+'</label>'+'</div>'+'<br>'+'<div class="respuesta">'+'<input '+' id=c'+num+''+' type="radio"name="'+num+'" value="c"><label class="respuesta_txt" for="c'+num+'">'+c+'</label>'+'</div>'+'<br>'+'<div class="respuesta">'+'<input '+' id=d'+num+''+' type="radio" name="'+num+'" value="d"><label class="respuesta_txt"for="d'+num+'">'+d+'</label>'+'</div></div>'+'<div class="imagen"><img width="100%" src="'+imagen+'"alt=""></div></div></div>';
     container.innerHTML += code1;
     //container.innerHTML += code2;
     preguntas += 1
     respuestas.push(respuesta)
 }
 
-//'+'<div class='texto'>texto</div>'+'
-
-//'+'width="100%"'+'
-
+document.getElementById('mensaje1').innerHTML += '<p>adffqfrhjkhjkawf</p>'
